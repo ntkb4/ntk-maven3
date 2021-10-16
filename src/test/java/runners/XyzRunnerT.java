ce4a375bb9f4d7f2aa1730fb16731133955fe84b
ntkb4 <ntkjasur@gmail.com>
2021-10-16 09:15:59 -0400

    INitial commit

---
+++ src/test/java/runners/XyzRunnerT.java
@@ -0,0 +1,24 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions (
+		
+			features = "src/test/resources/ntk1/", 
+			glue = {"steps" },
+			plugin = {"pretty", "html:target/cucumberd-report.html"},
+			monochrome = true
+	
+		)
+
+public class XyzRunnerT {
+	
+
+
+}
+ 
